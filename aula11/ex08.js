283e727b3a60407593bdaf16af269b6075b7d604
Breno Mendonça <209194550+mendoncabreno@users.noreply.github.com>
2025-08-14 19:25:29 -0300

    aula 11

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 45
+console.log(`A velocidade do seu carro é ${vel}KM/h` )
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permitida. Voce esta multado`)
+}
+console.log(`Dirija sempre usando sinto de segurança`)
